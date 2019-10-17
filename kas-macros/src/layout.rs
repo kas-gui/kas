a355fd2504d665206d9ac580d4e5d650c17ef645
Diggory Hardy <git@dhardy.name>
2019-10-17 16:55:57 +0100

    Compensate for rounding errors (horiz + vert); code simplification

--- kas-macros/src/layout.rs
+++ kas-macros/src/layout.rs
@@ -109,16 +109,6 @@ impl ImplLayout {
                     let mut size = Size::ZERO;
                 },
                 set_rect: quote! {
-                    let u0 = self.layout_total[0] as i64;
-                    let u1 = self.layout_total[1] as i64;
-                    let u = rect.size.0 as i64;
-                    let x = if u0 == u1 { 0.0 } else {
-                        (u - u0) as f64 / (u1 - u0) as f64
-                    };
-                    // println!("Horiz: u0={}, u1={}, u={}, x={}", u0, u1, u, x);
-                    assert!(0.0 <= x && x <= 1.0);
-                    let x1 = 1.0 - x;
-
                     let mut crect = rect;
                 },
             })
@@ -127,18 +117,10 @@ impl ImplLayout {
                 layout: Layout::Vert,
                 cols: 1,
                 rows: 0,
-                size: quote! {},
+                size: quote! {
+                    let mut size = Size::ZERO;
+                },
                 set_rect: quote! {
-                    let u0 = self.layout_total[0] as i64;
-                    let u1 = self.layout_total[1] as i64;
-                    let u = rect.size.1 as i64;
-                    let x = if u0 == u1 { 0.0 } else {
-                        (u - u0) as f64 / (u1 - u0) as f64
-                    };
-                    // println!("Vert: u0={}, u1={}, u={}, x={}", u0, u1, u, x);
-                    assert!(0.0 <= x && x <= 1.0);
-                    let x1 = 1.0 - x;
-
                     let mut crect = rect;
                 },
             })
@@ -168,19 +150,16 @@ impl ImplLayout {
                 self.size.append_all(quote! {
                     let child_size = self.#ident.size_pref(tk, pref, axes);
                     if axes != Axes::Vert {
-                        self.layout_sizes[#n + which] = child_size.0;
+                        self.layout_widths[#n + which] = child_size.0;
                     }
                     size.0 += child_size.0;
                     size.1 = std::cmp::max(size.1, child_size.1);
                 });
 
-                // This rounds down, which is fine except that a few pixels may go unused FIXME
                 self.set_rect.append_all(quote! {
-                    let u = (x1 * self.layout_sizes[#n] as f64
-                        + x * self.layout_sizes[#n + 1] as f64) as u32;
-                    crect.size.0 = u;
+                    crect.pos.0 = self.layout_widths[#n + 1] as i32;
+                    crect.size.0 = self.layout_widths[#n];
                     self.#ident.set_rect(crect);
-                    crect.pos.0 += u as i32;
                 });
             }
             Layout::Vert => {
@@ -190,19 +169,16 @@ impl ImplLayout {
                 self.size.append_all(quote! {
                     let child_size = self.#ident.size_pref(tk, pref, axes);
                     if axes != Axes::Horiz {
-                        self.layout_sizes[#n + which] = child_size.1;
+                        self.layout_heights[#n + which] = child_size.1;
                     }
                     size.0 = std::cmp::max(size.0, child_size.0);
                     size.1 += child_size.1;
                 });
 
-                // This rounds down, which is fine except that a few pixels may go unused FIXME
                 self.set_rect.append_all(quote! {
-                    let u = (x1 * self.layout_sizes[#n] as f64
-                        + x * self.layout_sizes[#n + 1] as f64) as u32;
-                    crect.size.1 = u;
+                    crect.pos.1 = self.layout_heights[#n + 1] as i32;
+                    crect.size.1 = self.layout_heights[#n];
                     self.#ident.set_rect(crect);
-                    crect.pos.1 += u as i32;
                 });
             }
             Layout::Grid => {
@@ -248,162 +224,153 @@ impl ImplLayout {
     pub fn finish(self) -> (TokenStream, TokenStream, TokenStream) {
         let cols = self.cols as usize;
         let rows = self.rows as usize;
+        let nc = cols * 2;
+        let nr = rows * 2;
         let size = self.size;
         let set_rect = self.set_rect;
 
-        let (fields, field_ctors);
-        match self.layout {
-            Layout::Horiz | Layout::Vert => {
-                let n = if self.layout == Layout::Horiz {
-                    cols * 2
-                } else {
-                    rows * 2
-                };
-                fields = quote! {
-                    layout_sizes: [u32; #n],
-                    layout_total: [u32; 2],
-                    layout_which: bool,
-                };
-                field_ctors = quote! {
-                    layout_sizes: [0; #n],
-                    layout_total: [0; 2],
-                    layout_which: false,
-                };
-            }
-            Layout::Grid => {
-                fields = quote! {
-                    layout_widths: [u32; #cols * 2],
-                    layout_heights: [u32; #rows * 2],
-                    layout_total: [kas::Size; 2],
-                    layout_which: bool,
-                };
-                field_ctors = quote! {
-                    layout_widths: [0; #cols * 2],
-                    layout_heights: [0; #rows * 2],
-                    layout_total: [kas::Size::ZERO; 2],
-                    layout_which: false,
-                };
-            }
+        let mut fields = quote! {
+            layout_which: bool,
+        };
+        let mut field_ctors = quote! {
+            layout_which: false,
+        };
+
+        if self.layout != Layout::Vert {
+            fields.append_all(quote! {
+                layout_widths: [u32; #nc + 2],
+            });
+            field_ctors.append_all(quote! {
+                layout_widths: [0; #nc + 2],
+            });
+        }
+        if self.layout != Layout::Horiz {
+            fields.append_all(quote! {
+                    layout_heights: [u32; #nr + 2],
+            });
+            field_ctors.append_all(quote! {
+                    layout_heights: [0; #nr + 2],
+            });
         }
 
-        let (size_pre, size_post, set_rect_pre);
-        match self.layout {
-            Layout::Horiz => {
-                size_pre = quote! {
-                    let mut size = Size::ZERO;
-                };
-                size_post = quote! {
-                    if axes != Axes::Vert {
-                        self.layout_total[which] = size.0;
-                    }
-                };
-                set_rect_pre = quote! {};
+        let size_pre = if self.layout != Layout::Grid {
+            quote! {
+                let mut size = Size::ZERO;
             }
-            Layout::Vert => {
-                size_pre = quote! {
-                    let mut size = Size::ZERO;
-                };
-                size_post = quote! {
-                    if axes != Axes::Horiz {
-                        self.layout_total[which] = size.1;
+        } else {
+            quote! {
+                if axes != Axes::Vert {
+                    for i in (0..#nc).step_by(2) {
+                        self.layout_widths[i + which] = 0;
                     }
-                };
-                set_rect_pre = quote! {};
+                }
+                if axes != Axes::Horiz {
+                    for i in (0..#nr).step_by(2) {
+                        self.layout_heights[i + which] = 0;
+                    }
+                }
             }
-            Layout::Grid => {
-                let nc = cols * 2;
-                let nr = rows * 2;
-                size_pre = quote! {
-                    if axes != Axes::Vert {
-                        for i in (0..#nc).step_by(2) {
-                            self.layout_widths[i + which] = 0;
-                        }
-                    }
-                    if axes != Axes::Horiz {
-                        for i in (0..#nr).step_by(2) {
-                            self.layout_heights[i + which] = 0;
-                        }
-                    }
-                };
-                size_post = quote! {
-                    let mut size = Size::ZERO;
+        };
+
+        let size_post = match self.layout {
+            Layout::Horiz => quote! {
+                if axes != Axes::Vert {
+                    self.layout_widths[#nc + which] = size.0;
+                }
+            },
+            Layout::Vert => quote! {
+                if axes != Axes::Horiz {
+                    self.layout_heights[#nr + which] = size.1;
+                }
+            },
+            Layout::Grid => quote! {
+                let mut size = Size::ZERO;
+                if axes != Axes::Vert {
                     for i in (0..#nc).step_by(2) {
                         size.0 += self.layout_widths[i + which];
                     }
+                    self.layout_widths[#nc + which] = size.0;
+                }
+                if axes != Axes::Horiz {
                     for i in (0..#nr).step_by(2) {
                         size.1 += self.layout_heights[i + which];
                     }
-                    if axes == Axes::Horiz {
-                        size.1 = self.layout_total[which].1;
-                    } else if axes == Axes::Vert {
-                        size.0 = self.layout_total[which].0;
-                    }
-                    self.layout_total[which] = size;
-                };
-                set_rect_pre = quote! {
-                    let u0 = self.layout_total[0].0 as i64;
-                    let u1 = self.layout_total[1].0 as i64;
-                    let u = rect.size.0 as i64;
-                    let x = if u0 == u1 { 0.0 } else {
-                        (u - u0) as f64 / (u1 - u0) as f64
-                    };
-                    // println!("Grid: u0={}, u1={}, u={}, x={}", u0, u1, u, x);
-                    let u0 = self.layout_total[0].1 as i64;
-                    let u1 = self.layout_total[1].1 as i64;
-                    let u = rect.size.1 as i64;
-                    let y = if u0 == u1 { 0.0 } else {
-                        (u - u0) as f64 / (u1 - u0) as f64
-                    };
-                    // println!("Grid: v0={}, v1={}, v={}, y={}", u0, u1, u, y);
-                    assert!(0.0 <= x && x <= 1.0);
-                    assert!(0.0 <= y && y <= 1.0);
-                    let x1 = 1.0 - x;
-                    let y1 = 1.0 - y;
-
-                    // Now calculate widths and cumulative widths
-                    let mut accum_w = 0;
-                    for i in (0..#nc).step_by(2) {
-                        let u = (x1 * self.layout_widths[i] as f64
-                            + x * self.layout_widths[i + 1] as f64) as u32;
-                        self.layout_widths[i] = u;
-                        self.layout_widths[i + 1] = accum_w;
-                        accum_w += u;
-                    }
-
-                    let mut accum_h = 0;
-                    for i in (0..#nr).step_by(2) {
-                        let u = (y1 * self.layout_heights[i] as f64
-                            + y * self.layout_heights[i + 1] as f64) as u32;
-                        self.layout_heights[i] = u;
-                        self.layout_heights[i + 1] = accum_h;
-                        accum_h += u;
-                    }
-
-                    // Assign excess from rounding errors to last rows/columns
-                    let excess = rect.size.0 - accum_w;
-                    let ex2 = 2 * excess as usize;
-                    assert!(excess >= 0 && ex2 <= #nc);
-                    accum_w = 0;
-                    for i in ((#nc - ex2)..#nc).step_by(2) {
-                        self.layout_widths[i] += 1;
-                        self.layout_widths[i + 1] += accum_w;
-                        accum_w += 1;
-                    }
-                    assert!(rect.size.0 == self.layout_widths[#nc - 1] + self.layout_widths[#nc - 2]);
-
-                    let excess = rect.size.1 - accum_h;
-                    let ex2 = 2 * excess as usize;
-                    assert!(excess >= 0 && ex2 <= #nr);
-                    accum_h = 0;
-                    for i in ((#nr - ex2)..#nr).step_by(2) {
-                        self.layout_heights[i] += 1;
-                        self.layout_heights[i + 1] += accum_h;
-                        accum_h += 1;
-                    }
-                    assert!(rect.size.1 == self.layout_heights[#nr - 1] + self.layout_heights[#nr - 2]);
-                };
-            }
+                    self.layout_heights[#nr + which] = size.1;
+                }
+            },
         };
+
+        let mut set_rect_pre = quote! {};
+        if self.layout != Layout::Vert {
+            set_rect_pre.append_all(quote! {
+                let u0 = self.layout_widths[#nc + 0] as i64;
+                let u1 = self.layout_widths[#nc + 1] as i64;
+                let u = rect.size.0 as i64;
+                let x = if u0 == u1 { 0.0 } else {
+                    (u - u0) as f64 / (u1 - u0) as f64
+                };
+                // println!("Grid: u0={}, u1={}, u={}, x={}", u0, u1, u, x);
+                assert!(0.0 <= x && x <= 1.0);
+                let x1 = 1.0 - x;
+
+                // Now calculate widths and cumulative widths
+                let mut accum_w = 0;
+                for i in (0..#nc).step_by(2) {
+                    let u = (x1 * self.layout_widths[i] as f64
+                        + x * self.layout_widths[i + 1] as f64) as u32;
+                    self.layout_widths[i] = u;
+                    self.layout_widths[i + 1] = accum_w;
+                    accum_w += u;
+                }
+
+                // Assign excess from rounding errors to last rows/columns
+                let excess = rect.size.0 - accum_w;
+                let ex2 = 2 * excess as usize;
+                assert!(excess >= 0 && ex2 <= #nc);
+                accum_w = 0;
+                for i in ((#nc - ex2)..#nc).step_by(2) {
+                    self.layout_widths[i] += 1;
+                    self.layout_widths[i + 1] += accum_w;
+                    accum_w += 1;
+                }
+                assert!(rect.size.0 == self.layout_widths[#nc - 1] + self.layout_widths[#nc - 2]);
+            });
+        }
+        if self.layout != Layout::Horiz {
+            set_rect_pre.append_all(quote! {
+                let u0 = self.layout_heights[#nr + 0] as i64;
+                let u1 = self.layout_heights[#nr + 1] as i64;
+                let u = rect.size.1 as i64;
+                let y = if u0 == u1 { 0.0 } else {
+                    (u - u0) as f64 / (u1 - u0) as f64
+                };
+                // println!("Grid: v0={}, v1={}, v={}, y={}", u0, u1, u, y);
+                assert!(0.0 <= y && y <= 1.0);
+                let y1 = 1.0 - y;
+
+                // Now calculate widths and cumulative widths
+                let mut accum_h = 0;
+                for i in (0..#nr).step_by(2) {
+                    let u = (y1 * self.layout_heights[i] as f64
+                        + y * self.layout_heights[i + 1] as f64) as u32;
+                    self.layout_heights[i] = u;
+                    self.layout_heights[i + 1] = accum_h;
+                    accum_h += u;
+                }
+
+                // Assign excess from rounding errors to last rows/columns
+                let excess = rect.size.1 - accum_h;
+                let ex2 = 2 * excess as usize;
+                assert!(excess >= 0 && ex2 <= #nr);
+                accum_h = 0;
+                for i in ((#nr - ex2)..#nr).step_by(2) {
+                    self.layout_heights[i] += 1;
+                    self.layout_heights[i + 1] += accum_h;
+                    accum_h += 1;
+                }
+                assert!(rect.size.1 == self.layout_heights[#nr - 1] + self.layout_heights[#nr - 2]);
+            });
+        }
 
         let fns = quote! {
             fn size_pref(&mut self, tk: &dyn kas::TkWidget, pref: kas::SizePref, axes: kas::Axes) -> kas::Size {
