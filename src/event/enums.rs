e4d7febaae166f8951348028de932af880a27deb
Diggory Hardy <git@dhardy.name>
2020-01-31 17:16:35 +0000

    Remove unused enums

--- src/event/enums.rs
+++ src/event/enums.rs
@@ -9,33 +9,6 @@
 
 #![allow(unused)]
 
-/// Describes the input state of a key.
-#[derive(Debug, Hash, PartialEq, Eq, Clone, Copy)]
-#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
-pub enum ElementState {
-    Pressed,
-    Released,
-}
-
-/// Represents the current state of the keyboard modifiers
-///
-/// Each field of this struct represents a modifier and is `true` if this modifier is active.
-#[derive(Default, Debug, Hash, PartialEq, Eq, Clone, Copy)]
-#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
-#[cfg_attr(feature = "serde", serde(default))]
-pub struct ModifiersState {
-    /// The "shift" key
-    pub shift: bool,
-    /// The "control" key
-    pub ctrl: bool,
-    /// The "alt" key
-    pub alt: bool,
-    /// The "logo" key
-    ///
-    /// This is the "windows" key on PC and "command" key on Mac.
-    pub logo: bool,
-}
-
 /// Describes a button of a mouse controller.
 #[derive(Debug, Hash, PartialEq, Eq, Clone, Copy)]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
