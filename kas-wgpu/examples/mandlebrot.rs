b153db2e3684baa9390a589638153d3c02ea3171
Diggory Hardy <git@dhardy.name>
2021-02-07 11:43:43 +0000

    Change front-face and enable culling
    Note: due to our text-centric "Y axis points down" system,
    clock-wise face order makes more sense than CCW.

--- kas-wgpu/examples/mandlebrot.rs
+++ kas-wgpu/examples/mandlebrot.rs
@@ -110,8 +110,8 @@ impl CustomPipeBuilder for PipeBuilder {
             primitive: wgpu::PrimitiveState {
                 topology: wgpu::PrimitiveTopology::TriangleList,
                 strip_index_format: None,
-                front_face: wgpu::FrontFace::Ccw,
-                cull_mode: wgpu::CullMode::None,
+                front_face: wgpu::FrontFace::Cw,
+                cull_mode: wgpu::CullMode::Back,
                 polygon_mode: wgpu::PolygonMode::Fill,
             },
             depth_stencil: Some(wgpu::DepthStencilState {
--- kas-wgpu/src/draw/shaded_square.rs
+++ kas-wgpu/src/draw/shaded_square.rs
@@ -110,8 +110,8 @@ impl Pipeline {
             primitive: wgpu::PrimitiveState {
                 topology: wgpu::PrimitiveTopology::TriangleList,
                 strip_index_format: None,
-                front_face: wgpu::FrontFace::Ccw,
-                cull_mode: wgpu::CullMode::None,
+                front_face: wgpu::FrontFace::Cw,
+                cull_mode: wgpu::CullMode::Back,
                 polygon_mode: wgpu::PolygonMode::Fill,
             },
             depth_stencil: Some(super::DEPTH_DESC),
