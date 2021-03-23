b4677963acfaab6a6321a60115e28f2862eaf4e0
Diggory Hardy <git@dhardy.name>
2021-03-23 11:24:22 +0000

    data-list-view: revise data structure (another fix)

--- kas-wgpu/examples/data-list-view.rs
+++ kas-wgpu/examples/data-list-view.rs
@@ -34,49 +34,45 @@ enum EntryMsg {
 #[derive(Debug)]
 struct MyData {
     len: usize,
-    active: usize,
-    map: RefCell<HashMap<usize, (bool, String)>>,
+    // (active index, map of strings)
+    data: RefCell<(usize, HashMap<usize, String>)>,
     handle: UpdateHandle,
 }
 impl MyData {
     fn new(len: usize) -> Self {
         MyData {
             len,
-            active: 0,
-            map: Default::default(),
+            data: Default::default(),
             handle: UpdateHandle::new(),
         }
     }
     fn set_len(&mut self, len: usize) -> (Option<String>, UpdateHandle) {
         self.len = len;
         let mut new_text = None;
-        if self.active >= len && len > 0 {
-            if let Some(value) = self.map.get_mut().get_mut(&self.active) {
-                value.0 = false;
-            }
-            self.active = len - 1;
-            if let Some(value) = self.map.get_mut().get_mut(&self.active) {
-                value.0 = true;
-            }
-            new_text = Some(self.get(self.active).1);
+        let mut data = self.data.borrow_mut();
+        if data.0 >= len && len > 0 {
+            let active = len - 1;
+            data.0 = active;
+            drop(data);
+            new_text = Some(self.get(active).1);
         }
         (new_text, self.handle)
     }
     fn get_active(&self) -> usize {
-        self.active
+        self.data.borrow().0
     }
     // Note: in general this method should update the data source and return
     // self.handle, but for our uses this is sufficient.
     fn set_active(&mut self, active: usize) -> String {
-        self.active = active;
+        self.data.borrow_mut().0 = active;
         self.get(active).1
     }
-    fn get(&self, n: usize) -> (bool, String) {
-        self.map
-            .borrow()
-            .get(&n)
-            .cloned()
-            .unwrap_or_else(|| (n == self.active, format!("Entry #{}", n + 1)))
+    fn get(&self, index: usize) -> (bool, String) {
+        let data = self.data.borrow();
+        let is_active = data.0 == index;
+        let text = data.1.get(&index).cloned();
+        let text = text.unwrap_or_else(|| format!("Entry #{}", index + 1));
+        (is_active, text)
     }
 }
 impl Updatable for MyData {
@@ -101,8 +97,12 @@ impl ListData for MyData {
         Some(self.get(*key))
     }
 
-    fn update(&self, key: &Self::Key, value: Self::Item) -> Option<UpdateHandle> {
-        self.map.borrow_mut().insert(key.clone(), value);
+    fn update(&self, key: &Self::Key, (is_active, text): Self::Item) -> Option<UpdateHandle> {
+        let mut data = self.data.borrow_mut();
+        if is_active {
+            data.0 = *key;
+        }
+        data.1.insert(*key, text);
         Some(self.handle)
     }
 
