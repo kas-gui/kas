512ccb5f4a4c19d5df56667cb9c9aef5da85a257
Diggory Hardy <git@dhardy.name>
2021-04-27 11:09:42 +0100

    Fix Label (nightly)

--- src/widget/label.rs
+++ src/widget/label.rs
@@ -56,13 +56,13 @@ impl Layout for AccelLabel {
 #[cfg(feature = "min_spec")]
 impl<'a> Layout for Label<&'a str> {
     fn draw(&self, draw_handle: &mut dyn DrawHandle, _: &ManagerState, _: bool) {
-        draw_handle.text(self.core.rect.pos, &self.label, TextClass::Label);
+        draw_handle.text(self.core.rect.pos, self.label.as_ref(), TextClass::Label);
     }
 }
 #[cfg(feature = "min_spec")]
 impl Layout for StringLabel {
     fn draw(&self, draw_handle: &mut dyn DrawHandle, _: &ManagerState, _: bool) {
-        draw_handle.text(self.core.rect.pos, &self.label, TextClass::Label);
+        draw_handle.text(self.core.rect.pos, self.label.as_ref(), TextClass::Label);
     }
 }
 
