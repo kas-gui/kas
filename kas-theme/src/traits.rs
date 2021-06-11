12841128738eae2eacd2e1a6bacb75676738a108
Diggory Hardy <git@dhardy.name>
2021-06-11 08:07:49 +0100

    Fix

--- kas-theme/src/traits.rs
+++ kas-theme/src/traits.rs
@@ -15,6 +15,9 @@ use std::ops::{Deref, DerefMut};
 pub trait ThemeConfig: Clone + std::fmt::Debug + 'static {
     /// Apply startup effects
     fn apply_startup(&self);
+
+    /// Get raster config
+    fn raster(&self) -> &crate::RasterConfig;
 }
 
 /// Requirements on theme config (with `config` feature)
