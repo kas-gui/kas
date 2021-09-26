151e9bf465bf2507893aa0d2a65d2f1534500869
Diggory Hardy <git@dhardy.name>
2021-09-26 20:13:47 +0100

    Revise counter examples

--- examples/counter.rs
+++ examples/counter.rs
@@ -5,43 +5,35 @@
 
 //! Counter example (simple button)
 
-use kas::macros::{make_widget, VoidMsg};
+use kas::macros::make_widget;
 use kas::prelude::*;
 use kas::widgets::{row, Label, TextButton, Window};
-
-#[derive(Clone, Debug, VoidMsg)]
-enum Message {
-    Decr,
-    Incr,
-}
 
 fn main() -> Result<(), kas::shell::Error> {
     env_logger::init();
 
-    let window = Window::new(
-        "Counter",
-        make_widget! {
-            #[layout(column)]
-            #[handler(msg = VoidMsg)]
-            struct {
-                #[widget(halign=centre)] display: Label<String> = Label::from("0"),
-                #[widget(use_msg = handle_button)] buttons -> Message = row![
-                    TextButton::new_msg("−", Message::Decr),
-                    TextButton::new_msg("+", Message::Incr),
-                ],
-                counter: usize = 0,
+    let counter = make_widget! {
+        #[layout(column)]
+        #[handler(msg = VoidMsg)]
+        struct {
+            #[widget(halign=centre)]
+            display: Label<String> = Label::from("0"),
+            #[widget(use_msg = handle_button)]
+            buttons = row![
+                TextButton::new_msg("−", -1),
+                TextButton::new_msg("+", 1),
+            ],
+            count: i32 = 0,
+        }
+        impl {
+            fn handle_button(&mut self, mgr: &mut Manager, incr: i32) {
+                self.count += incr;
+                *mgr |= self.display.set_string(self.count.to_string());
             }
-            impl {
-                fn handle_button(&mut self, mgr: &mut Manager, msg: Message) {
-                    match msg {
-                        Message::Decr => self.counter = self.counter.saturating_sub(1),
-                        Message::Incr => self.counter = self.counter.saturating_add(1),
-                    }
-                    *mgr |= self.display.set_string(self.counter.to_string());
-                }
-            }
-        },
-    );
+        }
+    };
+
+    let window = Window::new("Counter", counter);
 
     let theme = kas::theme::ShadedTheme::new().with_font_size(24.0);
     kas::shell::Toolkit::new(theme)?.with(window)?.run()
