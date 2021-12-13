e819fe085e576bc40a38097bf723531c7a1091bc
Diggory Hardy <git@dhardy.name>
2021-12-13 12:24:35 +0000

    Extra tests and bug fixes

--- crates/kas-core/src/core/widget_id.rs
+++ crates/kas-core/src/core/widget_id.rs
@@ -8,9 +8,9 @@
 use crate::cast::Conv;
 use std::fmt;
 use std::iter::once;
+use std::mem::size_of;
 use std::num::NonZeroU64;
 use std::sync::Mutex;
-use std::mem::size_of;
 
 /// Widget identifier
 ///
@@ -73,6 +73,7 @@ impl Iterator for BitsIter {
         }
         let (next, blocks) = next_from_bits(self.1);
         self.0 -= blocks;
+        self.1 <<= 4 * blocks;
         Some(next)
     }
     fn size_hint(&self) -> (usize, Option<usize>) {
@@ -82,41 +83,13 @@ impl Iterator for BitsIter {
 
 impl WidgetId {
     /// Identifier of the window
-    pub(crate) const ROOT: WidgetId = WidgetId(unsafe { NonZeroU64::new_unchecked(USE_DB) });
+    pub(crate) const ROOT: WidgetId = WidgetId(unsafe { NonZeroU64::new_unchecked(USE_BITS) });
 
     const INVALID: WidgetId = WidgetId(unsafe { NonZeroU64::new_unchecked(MASK_REST) });
 
     /// Returns true if `self` equals `id` or if `id` is a descendant of `self`
     pub fn is_ancestor_of(self, id: Self) -> bool {
-        let self_id = self.0.get();
-        let child_id = id.0.get();
-        if (child_id & USE_BITS) != 0 {
-            let self_blocks = block_len(self_id);
-            let child_blocks = block_len(child_id);
-            if (self_id & USE_BITS) == 0 || self_blocks > child_blocks {
-                return false;
-            }
-
-            let shift = 4 * (BLOCKS - self_blocks);
-            return (self_id & MASK_REST) >> shift == (child_id & MASK_REST) >> shift;
-        }
-
-        if (child_id & USE_DB) == 0 {
-            return false;
-        }
-
-        let db = DB.lock().unwrap();
-        let child_i = usize::conv(child_id & MASK_REST);
-
-        if (self_id & USE_BITS) != 0 {
-            let iter = BitsIter::new(self_id);
-            iter.zip(db[child_i].iter()).all(|(a, b)| a == *b)
-        } else if (self_id & USE_DB) != 0 {
-            let self_i = usize::conv(self_id & MASK_REST);
-            db[child_i].starts_with(&db[self_i])
-        } else {
-            false
-        }
+        self.index_of_child(id).is_some()
     }
 
     /// Get index of `child` relative to `self`
@@ -172,35 +145,38 @@ impl WidgetId {
     ///
     /// Note: this is not a getter method. Calling multiple times with the same
     /// `index` may or may not return the same value!
-    pub fn make_child(self, mut index: usize) -> Self {
+    pub fn make_child(self, index: usize) -> Self {
         let self_id = self.0.get();
         let mut path = None;
         if (self_id & USE_BITS) != 0 {
             // TODO(opt): this bit-packing approach is designed for space-optimisation, but it may
             // be better to use a simpler, less-compressed approach, possibly with u128 type.
             let block_len = block_len(self_id);
-            let avail_bits = 3 * (BLOCKS - block_len);
+            let avail_blocks = BLOCKS - block_len;
             let req_bits = 8 * size_of::<usize>() as u8 - index.leading_zeros() as u8;
-            if req_bits <= avail_bits {
-                let mut y = (index as u64 & 7) as u64;
+            if req_bits <= 3 * avail_blocks {
+                let mut x = index as u64;
+                let mut y = x & 7;
+                x >>= 3;
                 let mut shift = 4;
-                while index != 0 {
-                    y |= 1 << (shift - 1);
-                    y |= (index as u64 & 7) << shift;
-                    index >>= 3;
+                while x != 0 {
+                    y |= (8 | (x & 7)) << shift;
+                    x >>= 3;
                     shift += 4;
                 }
-                let used_blocks = (req_bits + 2) / 3;
+                // Note: zero is encoded with 1 block to force bump to len
+                let used_blocks = shift / 4;
+                debug_assert_eq!(used_blocks, ((req_bits + 2) / 3).max(1));
                 let len = (block_len as u64 + used_blocks as u64) << SHIFT_LEN;
-                let rest = y << BLOCKS - used_blocks;
-                let id = USE_BITS | len | rest;
+                let rest = y << 4 * avail_blocks - shift;
+                let id = USE_BITS | len | (self_id & MASK_REST) | rest;
                 return WidgetId(NonZeroU64::new(id).unwrap());
             } else {
                 path = Some(BitsIter::new(self_id).chain(once(index)).collect());
             }
         }
 
-        if (self_id & USE_DB) == 0 {
+        if (self_id & (USE_BITS | USE_DB)) == 0 {
             panic!("WidgetId::make_child: cannot make child of {}", self);
         }
 
@@ -273,8 +249,12 @@ impl fmt::Display for WidgetId {
         match self_id & MASK_HEAD {
             USE_BITS => {
                 let len = block_len(self_id);
-                let bits = (self_id & MASK_REST) >> (4 * (BLOCKS - len));
-                write!(f, "BITS#{1:0>0$x}", len as usize, bits)
+                if len == 0 {
+                    write!(f, "ROOT")
+                } else {
+                    let bits = (self_id & MASK_REST) >> (4 * (BLOCKS - len));
+                    write!(f, "BITS#{1:0>0$x}", len as usize, bits)
+                }
             }
             // TODO: encode as above?
             USE_DB => write!(f, "DB#{}", self_id & MASK_REST),
@@ -304,7 +284,10 @@ mod test {
         assert_eq!(next_from_bits(REST), (0, 1));
         assert_eq!(next_from_bits((7 << 60) | REST), (7, 1));
         assert_eq!(next_from_bits((0xB << 60) | (3 << 56)), (27, 2));
-        assert_eq!(next_from_bits(0xC9A4_F300_0000_0000), ((4 << 9) + (1<< 6) + (2<<3) + 4, 4));
+        assert_eq!(
+            next_from_bits(0xC9A4_F300_0000_0000),
+            ((4 << 9) + (1 << 6) + (2 << 3) + 4, 4)
+        );
     }
 
     #[test]
@@ -313,21 +296,95 @@ mod test {
             BitsIter::new(x).collect()
         }
         assert_eq!(as_vec(USE_BITS), vec![]);
-        assert_eq!(as_vec(0x87_1A93_007F_0000), vec![1, 139, 0, 0, 7]);
+        assert_eq!(as_vec(0x81_31_0000_0000_0000), vec![3]);
+        assert_eq!(as_vec(0x87_1A_9300_7F00_0000), vec![1, 139, 0, 0, 7]);
     }
 
     #[test]
     fn test_make_child() {
-        fn from_seq(seq: &[usize]) -> u64 {
+        fn test(seq: &[usize], x: u64) {
             let mut id = WidgetId::ROOT;
             for x in seq {
                 id = id.make_child(*x);
             }
-            id.into()
+            let v = u64::from(id);
+            if v != x {
+                panic!("test({:?}, {:x}): found {:x}", seq, x, v);
+            }
         }
 
-        assert_eq!(from_seq(&[]), USE_DB);
-        assert_eq!(from_seq(&[0, 0, 0]), USE_DB | (3 << SHIFT_LEN));
-        assert_eq!(from_seq(&[0, 1, 0]), USE_DB | (3 << SHIFT_LEN));
+        test(&[], USE_BITS);
+        test(&[0, 0, 0], USE_BITS | (3 << 56));
+        test(&[0, 1, 0], USE_BITS | (3 << 56) | (1 << 48));
+        test(&[9, 0, 1, 300], 0x879101cd40000000);
+        test(&[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 0], 0x8e12345679091920);
+    }
+
+    #[test]
+    fn test_display() {
+        fn from_seq(seq: &[usize]) -> String {
+            let mut id = WidgetId::ROOT;
+            for x in seq {
+                id = id.make_child(*x);
+            }
+            format!("{}", id)
+        }
+
+        assert_eq!(from_seq(&[]), "ROOT");
+        assert_eq!(from_seq(&[0]), "BITS#0");
+        assert_eq!(from_seq(&[1, 2, 3]), "BITS#123");
+        assert_eq!(from_seq(&[5, 9, 13]), "BITS#59195");
+        assert_eq!(from_seq(&[321]), "BITS#d81");
+        assert!(from_seq(&[313553, 13513, 13511631]).starts_with("DB#"));
+    }
+
+    #[test]
+    fn test_is_ancestor() {
+        fn test(seq: &[usize], seq2: &[usize]) {
+            let mut id = WidgetId::ROOT;
+            for x in seq {
+                id = id.make_child(*x);
+            }
+            println!("id={} val={:x} from {:?}", id, u64::from(id), seq);
+            let mut id2 = id;
+            for x in seq2 {
+                id2 = id2.make_child(*x);
+            }
+            println!("id2={} val={:x} from {:?}", id2, u64::from(id2), seq2);
+            let next = seq2.iter().next().cloned();
+            assert_eq!(id.index_of_child(id2), next);
+            assert_eq!(id.is_ancestor_of(id2), next.is_some());
+        }
+
+        test(&[], &[]);
+        test(&[], &[1]);
+        test(&[], &[51930, 2, 18]);
+        test(&[5, 6, 0, 1, 1], &[]);
+        test(&[5, 6, 0, 1, 1], &[1, 1]);
+        test(&[8, 26], &[0]);
+        test(&[9, 9, 9, 9, 9, 9, 9], &[]);
+        test(&[9, 9, 9, 9, 9, 9, 9], &[6]);
+        test(&[9, 9, 9, 9, 9, 9, 9, 9], &[3]);
+    }
+
+    #[test]
+    fn test_not_ancestor() {
+        fn test(seq: &[usize], seq2: &[usize]) {
+            let mut id = WidgetId::ROOT;
+            for x in seq {
+                id = id.make_child(*x);
+            }
+            println!("id={} val={:x} from {:?}", id, u64::from(id), seq);
+            let mut id2 = WidgetId::ROOT;
+            for x in seq2 {
+                id2 = id2.make_child(*x);
+            }
+            println!("id2={} val={:x} from {:?}", id2, u64::from(id2), seq2);
+            assert_eq!(id.index_of_child(id2), None);
+            assert_eq!(id.is_ancestor_of(id2), false);
+        }
+
+        test(&[0], &[]);
+        test(&[2, 10, 1], &[2, 10]);
     }
 }
