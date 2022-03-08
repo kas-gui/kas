0af82cc5e1d44ceccd1dca63e286b454beefe36b
Diggory Hardy <git@dhardy.name>
2022-03-08 11:46:26 +0000

    kas_macros: more tests

--- crates/kas-macros/tests/autoimpl.rs
+++ crates/kas-macros/tests/autoimpl.rs
@@ -1,5 +1,6 @@
 use kas_macros::autoimpl;
 use std::fmt::Debug;
+use std::marker::PhantomData;
 
 fn test_has_clone(_: impl Clone) {}
 fn test_has_debug(_: impl Debug) {}
@@ -11,4 +12,22 @@ struct Wrapper<T>(pub T);
 fn wrapper() {
     test_has_clone(Wrapper(0i32));
     test_has_debug(Wrapper(()));
+}
+
+#[autoimpl(Clone where A: Clone, B: Clone)]
+#[autoimpl(Debug where A: Debug)]
+struct X<A, B: Debug, C> {
+    a: A,
+    b: B,
+    c: PhantomData<C>,
+}
+
+#[test]
+fn x() {
+    let x = X {
+        a: 1i8,
+        b: "abc",
+        c: PhantomData::<fn()>,
+    };
+    test_has_debug(x.clone());
 }
