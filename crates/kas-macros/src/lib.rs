0c41b0004e91aba4fb7188b099a0c6fac82c37cf
Diggory Hardy <git@dhardy.name>
2022-03-11 09:10:10 +0000

    kas_macros::autoimpl: remove support for explicit definitive type

--- crates/kas-macros/src/lib.rs
+++ crates/kas-macros/src/lib.rs
@@ -145,12 +145,12 @@ mod widget_index;
 ///     fn f(&self) -> String;
 /// }
 /// ```
-/// Note that so long as a parameter bound like `T: trait` exists, the
-/// definitive type may be deduced (e.g. `<T as MyTrait>`). In the second
-/// case, we must specify the definitive type directly: `using dyn MyTrait`.
+/// Note that the first parameter bound like `T: trait` is used as the
+/// definitive type (required). For example, here, `f` is implemented with the
+/// body `<T as MyTrait>::f(self)`.
 ///
-/// Note also that parameters of trait re-implementations must start with `for`,
-/// hence the empty `for<>`.
+/// Note further: if the trait uses generic parameters itself, these must be
+/// introduced explicitly in the `for<..>` parameter list.
 
 #[proc_macro_attribute]
 #[proc_macro_error]
