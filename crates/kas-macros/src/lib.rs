3b139bb9a51c1040681a75df742bd80dd916b870
Diggory Hardy <git@dhardy.name>
2022-04-19 08:24:16 +0100

    kas-macros: refer to documentation from impl-tools instead of copying

--- crates/kas-macros/src/lib.rs
+++ crates/kas-macros/src/lib.rs
@@ -31,54 +31,8 @@ mod widget_index;
 
 /// Implement `Default`
 ///
-/// This macro may be used in one of two ways.
-///
-/// ### Type-level initialiser
-///
-/// ```
-/// # use kas_macros::impl_default;
-/// /// A simple enum; default value is Blue
-/// #[impl_default(Colour::Blue)]
-/// enum Colour {
-///     Red,
-///     Green,
-///     Blue,
-/// }
-///
-/// fn main() {
-///     assert!(matches!(Colour::default(), Colour::Blue));
-/// }
-/// ```
-///
-/// A where clause is optional: `#[impl_default(EXPR where BOUNDS)]`.
-///
-/// ### Field-level initialiser
-///
-/// This variant only supports structs. Fields specified as `name: type = expr`
-/// will be initialised with `expr`, while other fields will be initialised with
-/// `Defualt::default()`.
-///
-/// ```
-/// # use kas_macros::{impl_default, impl_scope};
-///
-/// impl_scope! {
-///     #[impl_default]
-///     struct Person {
-///         name: String = "Jane Doe".to_string(),
-///         age: u32 = 72,
-///         occupation: String,
-///     }
-/// }
-///
-/// fn main() {
-///     let person = Person::default();
-///     assert_eq!(person.name, "Jane Doe");
-///     assert_eq!(person.age, 72);
-///     assert_eq!(person.occupation, "");
-/// }
-/// ```
-///
-/// A where clause is optional: `#[impl_default(where BOUNDS)]`.
+/// See [`impl_tools::impl_default`](https://docs.rs/impl-tools/0.3/impl_tools/attr.impl_default.html)
+/// for full documentation.
 #[proc_macro_attribute]
 #[proc_macro_error]
 pub fn impl_default(attr: TokenStream, item: TokenStream) -> TokenStream {
@@ -96,131 +50,37 @@ pub fn impl_default(attr: TokenStream, item: TokenStream) -> TokenStream {
 
 /// A variant of the standard `derive` macro
 ///
-/// This macro is similar to `#[derive(Trait)]`, but with a few differences.
+/// See [`impl_tools::autoimpl`](https://docs.rs/impl-tools/0.3/impl_tools/attr.autoimpl.html)
+/// for full documentation.
 ///
-/// If using `autoimpl` **and** `derive` macros with Rust < 1.57.0, the
-/// `autoimpl` attribute must come first (see rust#81119).
+/// The following traits are supported:
 ///
-/// Unlike `derive`, `autoimpl` is not extensible by third-party crates. The
-/// "trait names" provided to `autoimpl` are matched directly, unlike
-/// `derive(...)` arguments which are paths to [`proc_macro_derive`] instances.
-/// Without language support for this there appears to be no option for
-/// third-party extensions.
+/// | Name | Path | *ignore* | *using* |
+/// |----- |----- |--- |--- |
+/// | `Clone` | `::std::clone::Clone` | initialized with `Default::default()` | - |
+/// | `Debug` | `::std::fmt::Debug` | field is not printed | - |
+/// | `Default` | `::std::default::Default` | - | - |
+/// | `Deref` | `::std::ops::Deref` | - | deref target |
+/// | `DerefMut` | `::std::ops::DerefMut` | - | deref target |
+/// | `Storage` | `::kas::layout::Storage` | - | - |
+/// | `HasBool` | `::kas::class::HasBool` | - | deref target |
+/// | `HasStr` | `::kas::class::HasStr` | - | deref target |
+/// | `HasString` | `::kas::class::HasString` | - | deref target |
+/// | `SetAccel` | `::kas::class::SetAccel` | - | deref target |
+/// | `class_traits` | each `kas::class` trait | - | deref target |
 ///
-/// [`proc_macro_derive`]: https://doc.rust-lang.org/reference/procedural-macros.html#derive-macros
+/// *Ignore:* trait supports ignoring fields (e.g. `#[autoimpl(Debug ignore self.foo)]`).
 ///
-/// ### Bounds on generic parameters
-///
-/// If a type has generic parameters, generated implementations will assume the
-/// same parameters and bounds as specified in the type, but not additional
-/// bounds for the trait implemented.
-///
-/// Additional bounds may be specified via a `where` clause. A special predicate
-/// is supported: `T: trait`; here `trait` is replaced the name of the trait
-/// being implemented.
-///
-/// # Multi-field traits
-///
-/// Some trait implementations make use of all fields (except those ignored):
-///
-/// -   `Clone` — implements `std::clone::Clone`; ignored fields are
-///     initialised with `Default::default()`
-/// -   `Debug` — implements `std::fmt::Debug`; ignored fields are not printed
-/// -   `Default` — implements `std::default::Default` using
-///     `Default::default()` for all fields (see also [`impl_default`](macro@impl_default))
-///
-/// ### Parameter syntax
-///
-/// > _ParamsMulti_ :\
-/// > &nbsp;&nbsp; ( _Trait_ ),+ _Ignores_? _WhereClause_?
-/// >
-/// > _Ignores_ :\
-/// > &nbsp;&nbsp; `ignore` ( `self` `.` _Member_ ),+
-/// >
-/// > _WhereClause_ :\
-/// > &nbsp;&nbsp; `where` ( _WherePredicate_ ),*
+/// *Using:* trait requires a named field to "use" (e.g. `#[autoimpl(Deref using self.foo)]`).
 ///
 /// ### Examples
 ///
-/// Implement `std::fmt::Debug`, ignoring the last field:
-/// ```
+/// Implement all `kas::class` trait over a wrapper type:
+/// ```no_test
 /// # use kas_macros::autoimpl;
-/// #[autoimpl(Debug ignore self.f)]
-/// struct PairWithFn<T> {
-///     x: f32,
-///     y: f32,
-///     f: fn(&T),
-/// }
+/// #[autoimpl(class_traits using self.0 where T: trait)]
+/// struct WrappingType<T>(T);
 /// ```
-///
-/// Implement `Clone` and `Debug` on a wrapper, with the required bounds:
-/// ```
-/// # use kas_macros::autoimpl;
-/// #[autoimpl(Clone, Debug where T: trait)]
-/// struct Wrapper<T>(pub T);
-/// ```
-/// Note: `T: trait` is a special predicate implying that for each
-/// implementation the type `T` must support the trait being implemented.
-///
-/// # Single-field traits
-///
-/// Other traits are implemented using a single field (for structs):
-///
-/// -   `Deref` — implements `std::ops::Deref`
-/// -   `DerefMut` — implements `std::ops::DerefMut`
-/// -   `HasBool`, `HasStr`, `HasString`, `SetAccel` — implement the `kas::class` traits
-/// -   `class_traits` — implements each `kas::class` trait (intended to be
-///     used with a where clause like `where W: trait`)
-///
-/// ### Parameter syntax
-///
-/// > _ParamsSingle_ :\
-/// > &nbsp;&nbsp; ( _Trait_ ),+ _Using_ _WhereClause_?
-/// >
-/// > _Using_ :\
-/// > &nbsp;&nbsp; `using` `self` `.` _Member_
-///
-/// ### Examples
-///
-/// Implement `Deref` and `DerefMut`, dereferencing to the given field:
-/// ```
-/// # use kas_macros::autoimpl;
-/// #[autoimpl(Deref, DerefMut using self.0)]
-/// struct MyWrapper<T>(T);
-/// ```
-///
-/// # Trait re-implementations
-///
-/// User-defined traits may be implemented over any type supporting `Deref`
-/// (and if required `DerefMut`) to another type supporting the trait.
-///
-/// ### Parameter syntax
-///
-/// > _ParamsTrait_ :\
-/// > &nbsp;&nbsp; `for` _Generics_ ( _Type_ ),+ _Definitive_? _WhereClause_?
-/// >
-/// > _Generics_ :\
-/// > &nbsp;&nbsp; `<` ( _GenericParam_ ) `>`
-/// >
-/// > _Definitive_ :\
-/// > &nbsp;&nbsp; `using` _Type_
-///
-/// ### Examples
-///
-/// Implement `MyTrait` for `&T`, `&mut T` and `Box<dyn MyTrait>`:
-/// ```
-/// # use kas_macros::autoimpl;
-/// #[autoimpl(for<'a, T: trait + ?Sized> &'a T, &'a mut T, Box<T>)]
-/// trait MyTrait {
-///     fn f(&self) -> String;
-/// }
-/// ```
-/// Note that the first parameter bound like `T: trait` is used as the
-/// definitive type (required). For example, here, `f` is implemented with the
-/// body `<T as MyTrait>::f(self)`.
-///
-/// Note further: if the trait uses generic parameters itself, these must be
-/// introduced explicitly in the `for<..>` parameter list.
 #[proc_macro_attribute]
 #[proc_macro_error]
 pub fn autoimpl(attr: TokenStream, item: TokenStream) -> TokenStream {
@@ -255,61 +115,8 @@ pub fn autoimpl(attr: TokenStream, item: TokenStream) -> TokenStream {
 
 /// Implementation scope
 ///
-/// Supports `impl Self` syntax.
-///
-/// Also supports struct field assignment syntax for `Default`: see [`impl_default`](macro@impl_default).
-///
-/// Caveat: `rustfmt` will not format contents (see
-/// [rustfmt#5254](https://github.com/rust-lang/rustfmt/issues/5254)).
-///
-/// ## Syntax
-///
-/// > _ImplScope_ :\
-/// > &nbsp;&nbsp; `impl_scope!` `{` _ScopeItem_ _ItemImpl_ * `}`
-/// >
-/// > _ScopeItem_ :\
-/// > &nbsp;&nbsp; _ItemEnum_ | _ItemStruct_ | _ItemType_ | _ItemUnion_
-///
-/// The result looks a little like a module containing a single type definition
-/// plus its implementations, but is injected into the parent module.
-///
-/// Implementations must target the type defined at the start of the scope. A
-/// special syntax for the target type, `Self`, is added:
-///
-/// > _ScopeImplItem_ :\
-/// > &nbsp;&nbsp; `impl` _GenericParams_? _ForTrait_? _ScopeImplTarget_ _WhereClause_? `{`
-/// > &nbsp;&nbsp; &nbsp;&nbsp; _InnerAttribute_*
-/// > &nbsp;&nbsp; &nbsp;&nbsp; _AssociatedItem_*
-/// > &nbsp;&nbsp; `}`
-/// >
-/// > _ScopeImplTarget_ :\
-/// > &nbsp;&nbsp; `Self` | _TypeName_ _GenericParams_?
-///
-/// That is, implementations may take one of two forms:
-///
-/// -   `impl MyType { ... }`
-/// -   `impl Self { ... }`
-///
-/// Generic parameters from the type are included automatically, with bounds as
-/// defined on the type. Additional generic parameters and an additional where
-/// clause are supported (generic parameter lists and bounds are merged).
-///
-/// ## Example
-///
-/// ```
-/// use kas_macros::impl_scope;
-/// use std::ops::Add;
-///
-/// impl_scope! {
-///     struct Pair<T>(T, T);
-///
-///     impl Self where T: Clone + Add {
-///         fn sum(&self) -> <T as Add>::Output {
-///             self.0.clone().add(self.1.clone())
-///         }
-///     }
-/// }
-/// ```
+/// See [`impl_tools::impl_scope`](https://docs.rs/impl-tools/0.3/impl_tools/macro.impl_scope.html)
+/// for full documentation.
 #[proc_macro_error]
 #[proc_macro]
 pub fn impl_scope(input: TokenStream) -> TokenStream {
