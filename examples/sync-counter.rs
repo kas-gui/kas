b45416d7813fcec4a770e298e433c64aaafea63d
Diggory Hardy <git@dhardy.name>
2022-07-14 16:35:23 +0100

    Simplify examples/sync-counter

--- examples/sync-counter.rs
+++ examples/sync-counter.rs
@@ -5,43 +5,20 @@
 
 //! A counter synchronised between multiple windows
 
-use kas::macros::impl_scope;
 use kas::model::SharedRc;
+use kas::widgets::dialog::Window;
 use kas::widgets::view::{driver, SingleView};
-use kas::Window;
 
 fn main() -> kas::shell::Result<()> {
     env_logger::init();
 
-    impl_scope! {
-        #[widget{
-            layout = self.spinner;
-        }]
-        #[derive(Debug)]
-        struct Counter {
-            core: widget_core!(),
-            // SingleView embeds a shared value, here default-constructed to 0
-            #[widget] spinner: SingleView<SharedRc<i32>, driver::Spinner<i32>>,
-        }
-        impl Self {
-            fn new(data: SharedRc<i32>) -> Self {
-                let driver = driver::Spinner::new(i32::MIN..=i32::MAX, 1);
-                Counter {
-                    core: Default::default(),
-                    spinner: SingleView::new_with_driver(driver, data),
-                }
-            }
-        }
-        impl Window for Self {
-            fn title(&self) -> &str { "Counter" }
-        }
-    };
-
-    let data = SharedRc::new(0);
+    let driver = driver::Spinner::new(i32::MIN..=i32::MAX, 1);
+    let c1 = SingleView::new_with_driver(driver, SharedRc::new(0));
+    let c2 = SingleView::new_with_driver(driver, c1.data().clone());
 
     let theme = kas::theme::ShadedTheme::new().with_font_size(24.0);
     kas::shell::Toolkit::new(theme)?
-        .with(Counter::new(data.clone()))?
-        .with(Counter::new(data))?
+        .with(Window::new("Counter 1", c1))?
+        .with(Window::new("Counter 2", c2))?
         .run()
 }
