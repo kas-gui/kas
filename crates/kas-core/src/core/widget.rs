7729a24cde4731b5d83eb95e907b4ff5d725e2cf
Diggory Hardy <git@dhardy.name>
2022-08-09 16:02:10 +0100

    Widget::nav_next doc

--- crates/kas-core/src/core/widget.rs
+++ crates/kas-core/src/core/widget.rs
@@ -472,8 +472,10 @@ pub trait Widget: WidgetChildren + Layout {
     /// Both `from` and the return value use the widget index, as used by
     /// [`WidgetChildren::get_child`].
     ///
-    /// The default implementation often suffices: it will navigate through
-    /// children in order.
+    /// Default implementation:
+    ///
+    /// -   Generated from `#[widget]`'s layout property, if used
+    /// -   Otherwise, iterate through children in order of definition
     #[inline]
     fn nav_next(
         &mut self,
