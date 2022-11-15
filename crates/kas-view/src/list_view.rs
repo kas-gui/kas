43bbba5faeedd90b8220dc60b645c39a773ad220
Diggory Hardy <git@dhardy.name>
2022-11-15 11:08:21 +0000

    Fix for view widgets: reset state when changing data binding

--- crates/kas-view/src/list_view.rs
+++ crates/kas-view/src/list_view.rs
@@ -374,7 +374,9 @@ impl_scope! {
                 let id = self.data.make_id(self.id_ref(), &key);
                 let w = &mut self.widgets[i % solver.cur_len];
                 if w.key.as_ref() != Some(&key) {
-                    // TODO(opt): we only need to configure the widget once
+                    // Reset widgets to ensure input state such as cursor
+                    // position does not bleed over to next data entry
+                    w.widget = self.driver.make();
                     mgr.configure(id, &mut w.widget);
 
                     if let Some(item) = self.data.borrow(&key) {
--- crates/kas-view/src/matrix_view.rs
+++ crates/kas-view/src/matrix_view.rs
@@ -350,7 +350,11 @@ impl_scope! {
                     let id = self.data.make_id(self.id_ref(), &key);
                     let w = &mut self.widgets[i];
                     if w.key.as_ref() != Some(&key) {
+                        // Reset widgets to ensure input state such as cursor
+                        // position does not bleed over to next data entry
+                        w.widget = self.driver.make();
                         mgr.configure(id, &mut w.widget);
+
                         if let Some(item) = self.data.borrow(&key) {
                             action |= self.driver.set(&mut w.widget, &key, item.borrow());
                             w.key = Some(key);
