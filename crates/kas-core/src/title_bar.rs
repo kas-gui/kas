8d8fc8af3b115724f2a85c822ff8f176349a5e0d
Diggory Hardy <git@dhardy.name>
2023-02-09 08:47:08 +0000

    Move TitleBar to kas-core

---
+++ crates/kas-core/src/title_bar.rs
@@ -0,0 +1,158 @@
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License in the LICENSE-APACHE file or at:
+//     https://www.apache.org/licenses/LICENSE-2.0
+
+//! Title bar
+//!
+//! Note: due to definition in kas-core, some widgets must be duplicated.
+
+use crate::event::ConfigMgr;
+use crate::geom::Rect;
+use crate::layout::{Align, AxisInfo, SizeRules};
+use crate::text::Text;
+use crate::theme::{DrawMgr, SizeMgr, TextClass};
+use crate::{Layout, WidgetCore};
+use kas::prelude::*;
+use kas::theme::MarkStyle;
+use kas_macros::impl_scope;
+use std::fmt::Debug;
+
+impl_scope! {
+    /// A simple label
+    #[derive(Clone, Debug, Default)]
+    #[widget]
+    pub struct Label {
+        core: widget_core!(),
+        label: Text<String>,
+    }
+
+    impl Self {
+        /// Construct from `label`
+        #[inline]
+        fn new(label: String) -> Self {
+            Label {
+                core: Default::default(),
+                label: Text::new(label),
+            }
+        }
+
+        /// Text class
+        pub const CLASS: TextClass = TextClass::Label(false);
+    }
+
+    impl Layout for Self {
+        #[inline]
+        fn size_rules(&mut self, size_mgr: SizeMgr, mut axis: AxisInfo) -> SizeRules {
+            axis.set_default_align_hv(Align::Center, Align::Center);
+            size_mgr.text_rules(&mut self.label, Self::CLASS, axis)
+        }
+
+        fn set_rect(&mut self, mgr: &mut ConfigMgr, rect: Rect) {
+            self.core.rect = rect;
+            mgr.text_set_size(&mut self.label, Self::CLASS, rect.size, None);
+        }
+
+        fn draw(&mut self, mut draw: DrawMgr) {
+            draw.text(self.rect(), &self.label, Self::CLASS);
+        }
+    }
+}
+
+impl_scope! {
+    /// A mark which is also a button
+    ///
+    /// This button is not keyboard navigable; only mouse/touch interactive.
+    ///
+    /// Uses stretch policy [`Stretch::Low`].
+    #[derive(Clone, Debug)]
+    #[widget {
+        hover_highlight = true;
+    }]
+    pub struct MarkButton<M: Clone + Debug + 'static> {
+        core: widget_core!(),
+        style: MarkStyle,
+        msg: M,
+    }
+
+    impl Self {
+        /// Construct
+        ///
+        /// A clone of `msg` is sent as a message on click.
+        pub fn new(style: MarkStyle, msg: M) -> Self {
+            MarkButton {
+                core: Default::default(),
+                style,
+                msg,
+            }
+        }
+    }
+
+    impl Layout for Self {
+        fn size_rules(&mut self, mgr: SizeMgr, axis: AxisInfo) -> SizeRules {
+            mgr.feature(self.style.into(), axis)
+        }
+
+        fn draw(&mut self, mut draw: DrawMgr) {
+            draw.mark(self.core.rect, self.style);
+        }
+    }
+
+    impl Widget for Self {
+        fn handle_event(&mut self, mgr: &mut EventMgr, event: Event) -> Response {
+            event.on_activate(mgr, self.id(), |mgr| {
+                mgr.push(self.msg.clone());
+                Response::Used
+            })
+        }
+    }
+}
+
+#[derive(Copy, Clone, Debug)]
+enum TitleBarButton {
+    Minimize,
+    Maximize,
+    Close,
+}
+
+impl_scope! {
+    /// A window's title bar (part of decoration)
+    #[derive(Clone, Debug, Default)]
+    #[widget{
+        layout = row: [
+            // self.icon,
+            self.title,
+            MarkButton::new(MarkStyle::Point(Direction::Down), TitleBarButton::Minimize),
+            MarkButton::new(MarkStyle::Point(Direction::Up), TitleBarButton::Maximize),
+            MarkButton::new(MarkStyle::X, TitleBarButton::Close),
+        ];
+    }]
+    pub struct TitleBar {
+        core: widget_core!(),
+        #[widget]
+        title: Label,
+    }
+
+    impl Self {
+        /// Construct a title bar
+        #[inline]
+        pub fn new(title: String) -> Self {
+            TitleBar {
+                core: Default::default(),
+                title: Label::new(title),
+            }
+        }
+    }
+
+    impl Widget for Self {
+        fn handle_message(&mut self, mgr: &mut EventMgr) {
+            if let Some(msg) = mgr.try_pop() {
+                match msg {
+                    TitleBarButton::Minimize => todo!(),
+                    TitleBarButton::Maximize => todo!(),
+                    TitleBarButton::Close => mgr.send_action(Action::CLOSE),
+                }
+            }
+        }
+    }
+}
--- crates/kas-widgets/src/lib.rs
+++ crates/kas-widgets/src/lib.rs
@@ -73,7 +73,6 @@ mod spinner;
 mod splitter;
 mod stack;
 mod tab_stack;
-mod title_bar;
 
 pub mod adapter;
 
