8e25a083962763188cf85c0b34382b60f0c47c7b
Diggory Hardy <git@dhardy.name>
2023-04-20 08:49:20 +0100

    check.sh: stop if first check fails

--- check.sh
+++ check.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-# Check all examples not using view widgets:
-cargo check --example async-event --example calculator --example clock --example counter --example cursors  --example hello --example splitter
-# Except these:
+# Check all examples not using view widgets except these:
 # --example data-list --example stopwatch --example layout
-cargo check --manifest-path examples/mandlebrot/Cargo.toml
+cargo check --example async-event --example calculator --example clock --example counter --example cursors  --example hello --example splitter \
+&& cargo check --manifest-path examples/mandlebrot/Cargo.toml
