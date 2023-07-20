952969e262d6fc9eb32d535182327266f9128b58
Diggory Hardy <git@dhardy.name>
2023-07-20 10:39:01 +0100

    FilteredList: use impl_scope and impl Self syntax

--- crates/kas-view/src/filter/filter_list.rs
+++ crates/kas-view/src/filter/filter_list.rs
@@ -7,97 +7,100 @@
 
 use super::Filter;
 use crate::{ListData, SharedData};
+use kas::impl_scope;
 use std::borrow::Borrow;
 use std::cell::{Ref, RefCell};
 use std::fmt::Debug;
 
-/// Filter accessor over another accessor
-///
-/// This is an abstraction over a [`ListData`], applying a filter to items when
-/// iterating and accessing.
-///
-/// When updating, the filter applies to the old value: if the old is included,
-/// it is replaced by the new, otherwise no replacement occurs.
-///
-/// Note: the key and item types are the same as those in the underlying list,
-/// thus one can also retrieve values from the underlying list directly.
-///
-/// Warning: this implementation is `O(n)` where `n = data.len()` and not well
-/// optimised, thus is expected to be slow on large data lists.
-#[derive(Clone, Debug)]
-pub struct FilteredList<T: ListData, F: Filter<T::Item> + Debug> {
-    /// Direct access to unfiltered data
+impl_scope! {
+    /// Filter accessor over another accessor
     ///
-    /// If adjusting this, one should call [`FilteredList::refresh`] after.
-    data: T,
-    /// Direct access to the filter
+    /// This is an abstraction over a [`ListData`], applying a filter to items when
+    /// iterating and accessing.
     ///
-    /// If adjusting this, one should call [`FilteredList::refresh`] after.
-    filter: F,
-    view: RefCell<(u64, Vec<(T::Key, T::Version)>)>,
-}
-
-impl<T: ListData, F: Filter<T::Item> + Debug> FilteredList<T, F> {
-    /// Construct from `data` and a `filter`
-    #[inline]
-    pub fn new(data: T, filter: F) -> Self {
-        let len = data.len();
-        let view = RefCell::new((0, Vec::with_capacity(len)));
-        FilteredList { data, filter, view }
+    /// When updating, the filter applies to the old value: if the old is included,
+    /// it is replaced by the new, otherwise no replacement occurs.
+    ///
+    /// Note: the key and item types are the same as those in the underlying list,
+    /// thus one can also retrieve values from the underlying list directly.
+    ///
+    /// Warning: this implementation is `O(n)` where `n = data.len()` and not well
+    /// optimised, thus is expected to be slow on large data lists.
+    #[derive(Clone, Debug)]
+    pub struct FilteredList<T: ListData, F: Filter<T::Item> + Debug> {
+        /// Direct access to unfiltered data
+        ///
+        /// If adjusting this, one should call [`FilteredList::refresh`] after.
+        data: T,
+        /// Direct access to the filter
+        ///
+        /// If adjusting this, one should call [`FilteredList::refresh`] after.
+        filter: F,
+        view: RefCell<(u64, Vec<(T::Key, T::Version)>)>,
     }
 
-    /// Refresh the view
-    ///
-    /// Re-applies the filter (`O(n)` where `n` is the number of data elements).
-    /// Calling this directly may be useful in case the data is modified.
-    fn refresh(&self, ver: u64) {
-        let mut view = self.view.borrow_mut();
-        view.0 = ver;
-        view.1.clear();
-        for (key, version) in self.data.iter_from(0, usize::MAX) {
-            if let Some(item) = self.data.borrow(&key) {
-                if self.filter.matches(item.borrow()) {
-                    view.1.push((key, version));
+    impl Self {
+        /// Construct from `data` and a `filter`
+        #[inline]
+        pub fn new(data: T, filter: F) -> Self {
+            let len = data.len();
+            let view = RefCell::new((0, Vec::with_capacity(len)));
+            FilteredList { data, filter, view }
+        }
+
+        /// Refresh the view
+        ///
+        /// Re-applies the filter (`O(n)` where `n` is the number of data elements).
+        /// Calling this directly may be useful in case the data is modified.
+        fn refresh(&self, ver: u64) {
+            let mut view = self.view.borrow_mut();
+            view.0 = ver;
+            view.1.clear();
+            for (key, version) in self.data.iter_from(0, usize::MAX) {
+                if let Some(item) = self.data.borrow(&key) {
+                    if self.filter.matches(item.borrow()) {
+                        view.1.push((key, version));
+                    }
                 }
             }
         }
     }
-}
 
-impl<T: ListData, F: Filter<T::Item> + Debug> SharedData for FilteredList<T, F> {
-    type Key = T::Key;
-    type Version = T::Version;
-    type Item = T::Item;
-    type ItemRef<'b> = T::ItemRef<'b> where T: 'b;
+    impl SharedData for Self {
+        type Key = T::Key;
+        type Version = T::Version;
+        type Item = T::Item;
+        type ItemRef<'b> = T::ItemRef<'b> where T: 'b;
 
-    fn contains_key(&self, key: &Self::Key) -> bool {
-        SharedData::borrow(self, key).is_some()
-    }
-    fn borrow(&self, key: &Self::Key) -> Option<Self::ItemRef<'_>> {
-        // Check the item against our filter (probably O(1)) instead of using
-        // our filtered list (O(n) where n=self.len()).
-        self.data
-            .borrow(key)
-            .filter(|item| self.filter.matches(item.borrow()))
-    }
-}
-
-impl<T: ListData, F: Filter<T::Item> + Debug> ListData for FilteredList<T, F> {
-    type KeyIter<'b> = KeyIter<'b, (T::Key, T::Version)>
-    where Self: 'b;
-
-    fn is_empty(&self) -> bool {
-        self.view.borrow().1.is_empty()
-    }
-    fn len(&self) -> usize {
-        self.view.borrow().1.len()
+        fn contains_key(&self, key: &Self::Key) -> bool {
+            SharedData::borrow(self, key).is_some()
+        }
+        fn borrow(&self, key: &Self::Key) -> Option<Self::ItemRef<'_>> {
+            // Check the item against our filter (probably O(1)) instead of using
+            // our filtered list (O(n) where n=self.len()).
+            self.data
+                .borrow(key)
+                .filter(|item| self.filter.matches(item.borrow()))
+        }
     }
 
-    fn iter_from(&self, start: usize, limit: usize) -> Self::KeyIter<'_> {
-        let end = self.len().min(start + limit);
-        let borrow = Ref::map(self.view.borrow(), |tuple| &tuple.1[start..end]);
-        let index = 0;
-        KeyIter { borrow, index }
+    impl ListData for Self {
+        type KeyIter<'b> = KeyIter<'b, (T::Key, T::Version)>
+        where Self: 'b;
+
+        fn is_empty(&self) -> bool {
+            self.view.borrow().1.is_empty()
+        }
+        fn len(&self) -> usize {
+            self.view.borrow().1.len()
+        }
+
+        fn iter_from(&self, start: usize, limit: usize) -> Self::KeyIter<'_> {
+            let end = self.len().min(start + limit);
+            let borrow = Ref::map(self.view.borrow(), |tuple| &tuple.1[start..end]);
+            let index = 0;
+            KeyIter { borrow, index }
+        }
     }
 }
 
