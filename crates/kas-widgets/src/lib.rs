d9a8156b59ad565ad73d133a297e6c40301345c6
Diggory Hardy <git@dhardy.name>
2023-09-01 10:22:32 +0100

    Remove redundant doc-links

--- crates/kas-widgets/src/lib.rs
+++ crates/kas-widgets/src/lib.rs
@@ -8,8 +8,8 @@
 //! ## Complex widgets
 //!
 //! -   [`EventConfig`] provides an editor for event configuration
-//! -   [`kas::Window`](kas::Window) is the root of any UI tree used as a window
-//! -   [`kas::Popup`](kas::Popup) is the root of any popup
+//! -   [`kas::Window`] is the root of any UI tree used as a window
+//! -   [`kas::Popup`] is the root of any popup
 //!
 //! ## Sub-modules
 //!
