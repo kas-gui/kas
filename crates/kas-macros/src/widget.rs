b1111e37abe58c4e870064dcd0d55fbab5f37715
Diggory Hardy <git@dhardy.name>
2024-12-10 17:14:07 +0000

    Allow Widget impls to specify _nav_next

--- crates/kas-macros/src/widget.rs
+++ crates/kas-macros/src/widget.rs
@@ -364,6 +364,10 @@ pub fn widget(attr_span: Span, mut args: WidgetArgs, scope: &mut Scope) -> Resul
                 .items
                 .push(Verbatim(widget_recursive_methods(&core_path)));
         }
+
+        if !has_item("_nav_next") {
+            widget_impl.items.push(Verbatim(widget_nav_next()));
+        }
     } else {
         scope.generated.push(impl_widget(
             &impl_generics,
@@ -737,6 +741,7 @@ pub fn impl_widget(
     };
 
     let fns_recurse = widget_recursive_methods(core_path);
+    let fn_nav_next = widget_nav_next();
 
     quote! {
         impl #impl_generics ::kas::Widget for #impl_target {
@@ -744,6 +749,7 @@ pub fn impl_widget(
             #fns_as_node
             #fns_for_child
             #fns_recurse
+            #fn_nav_next
         }
     }
 }
@@ -804,7 +810,11 @@ fn widget_recursive_methods(core_path: &Toks) -> Toks {
         ) {
             ::kas::impls::_replay(self, cx, data, id);
         }
+    }
+}
 
+fn widget_nav_next() -> Toks {
+    quote! {
         fn _nav_next(
             &mut self,
             cx: &mut ::kas::event::ConfigCx,
