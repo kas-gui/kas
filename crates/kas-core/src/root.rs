f52da1c108cc22dc3b30accdb86237a16ecace65
Diggory Hardy <git@dhardy.name>
2025-06-21 15:33:15 +0100

    Generate list of AccessKit nodes

--- crates/kas-core/src/root.rs
+++ crates/kas-core/src/root.rs
@@ -482,6 +482,11 @@ impl<Data: 'static> Window<Data> {
             self.resize_popup(cx, data, i);
         }
     }
+
+    /// Iterate over popups
+    pub(crate) fn iter_popups(&self) -> impl Iterator<Item = &kas::PopupDescriptor> {
+        self.popups.iter().map(|(_, popup, _)| popup)
+    }
 }
 
 impl<Data: 'static> Window<Data> {
