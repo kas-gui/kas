15244f75a40df19bbad02b1ad4f778e1ce741bd8
Diggory Hardy <git@dhardy.name>
2025-12-01 09:31:17 +0000

    Introduce tile State

--- examples/file-explorer/src/tile.rs
+++ examples/file-explorer/src/tile.rs
@@ -1,18 +1,55 @@
 use crate::Entry;
 use kas::prelude::*;
-use kas::widgets::{Page, Stack, Text};
+use kas::widgets::{Adapt, Page, Stack, Text};
+use std::borrow::Cow;
+use std::path::PathBuf;
 
-fn generic() -> impl Widget<Data = Entry> {
-    Text::new_gen(|_, entry: &Entry| match &entry {
-        Ok(path) => format!("{}", path.display()),
-        Err(err) => format!("Error: {err}"),
+#[autoimpl(Debug)]
+pub enum State {
+    Initial,
+    Error(String),
+    Unknown(PathBuf),
+}
+
+impl State {
+    fn update(&mut self, entry: &Entry) {
+        let Ok(path) = entry else {
+            *self = State::Error(format!("Error: {:?}", entry.as_ref().unwrap_err()));
+            return;
+        };
+
+        if matches!(self, State::Unknown(p) if p == path) {
+            return;
+        }
+
+        *self = State::Unknown(path.clone());
+    }
+}
+
+fn generic() -> impl Widget<Data = State> {
+    Text::new_update(|_, entry: &State, text: &mut String| {
+        let new_text: Cow<str> = match &entry {
+            State::Initial => Cow::from("loading"),
+            State::Error(text) => text.into(),
+            State::Unknown(path) => format!("{}", path.display()).into(),
+        };
+        if *text != new_text {
+            *text = new_text.into_owned();
+            true
+        } else {
+            false
+        }
     })
 }
 
-type Tile = Stack<Entry>;
+type Tile = Adapt<Entry, Stack<State>>;
 
 fn tile() -> Tile {
     Stack::from([Page::new(generic())])
+        .with_state(State::Initial)
+        .on_update(|_, state, entry| {
+            state.update(entry);
+        })
 }
 
 #[derive(Default)]
