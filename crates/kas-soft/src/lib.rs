168ca48a006c696b578cbc712b3fa03b63591855
Diggory Hardy <git@dhardy.name>
2025-12-02 13:39:14 +0000

    Add kas-soft crate

---
+++ crates/kas-soft/src/lib.rs
@@ -0,0 +1,12 @@
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License in the LICENSE-APACHE file or at:
+//     https://www.apache.org/licenses/LICENSE-2.0
+
+//! KAS graphics backend over [softbuffer]
+//!
+//! This crate implements a KAS's drawing APIs over [softbuffer].
+//!
+//! This crate supports themes via the [`kas::theme`].
+//!
+//! [softbuffer]: https://github.com/rust-windowing/softbuffer
