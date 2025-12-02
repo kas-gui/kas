84f7f10fc53e35b9c78781d4d1a91ff4bfb24e00
Diggory Hardy <git@dhardy.name>
2025-12-02 15:43:51 +0000

    kas-soft: add empty interface impls

---
+++ crates/kas-soft/src/draw.rs
@@ -0,0 +1,109 @@
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License in the LICENSE-APACHE file or at:
+//     https://www.apache.org/licenses/LICENSE-2.0
+
+//! KAS graphics backend over [softbuffer]
+//!
+//! This crate implements a KAS's drawing APIs over [softbuffer].
+//!
+//! This crate supports themes via the [`kas::theme`].
+//!
+//! [softbuffer]: https://github.com/rust-windowing/softbuffer
+
+use kas::draw::{AllocError, DrawImpl, DrawSharedImpl, PassId, PassType, WindowCommon};
+use kas::draw::{ImageFormat, ImageId, color};
+use kas::geom::{Quad, Vec2};
+use kas::prelude::{Offset, Rect};
+use kas::runner::{self, RunError};
+use kas::text;
+use raw_window_handle::{HasDisplayHandle, HasWindowHandle};
+
+pub struct Draw;
+
+impl DrawImpl for Draw {
+    fn common_mut(&mut self) -> &mut WindowCommon {
+        todo!()
+    }
+
+    fn new_pass(
+        &mut self,
+        parent_pass: PassId,
+        rect: Rect,
+        offset: Offset,
+        class: PassType,
+    ) -> PassId {
+        todo!()
+    }
+
+    fn get_clip_rect(&self, pass: PassId) -> Rect {
+        todo!()
+    }
+
+    fn rect(&mut self, pass: PassId, rect: Quad, col: color::Rgba) {
+        todo!()
+    }
+
+    fn frame(&mut self, pass: PassId, outer: Quad, inner: Quad, col: color::Rgba) {
+        todo!()
+    }
+}
+
+pub struct Shared {}
+
+impl DrawSharedImpl for Shared {
+    type Draw = Draw;
+
+    fn max_texture_dimension_2d(&self) -> u32 {
+        todo!()
+    }
+
+    fn set_raster_config(&mut self, config: &kas::config::RasterConfig) {
+        todo!()
+    }
+
+    fn image_alloc(&mut self, size: (u32, u32)) -> Result<ImageId, AllocError> {
+        todo!()
+    }
+
+    fn image_upload(&mut self, id: ImageId, data: &[u8], format: ImageFormat) {
+        todo!()
+    }
+
+    fn image_free(&mut self, id: ImageId) {
+        todo!()
+    }
+
+    fn image_size(&self, id: ImageId) -> Option<(u32, u32)> {
+        todo!()
+    }
+
+    fn draw_image(&self, draw: &mut Draw, pass: PassId, id: ImageId, rect: Quad) {
+        todo!()
+    }
+
+    fn draw_text(
+        &mut self,
+        draw: &mut Draw,
+        pass: PassId,
+        pos: Vec2,
+        bb: Quad,
+        text: &text::TextDisplay,
+        col: color::Rgba,
+    ) {
+        todo!()
+    }
+
+    fn draw_text_effects(
+        &mut self,
+        draw: &mut Draw,
+        pass: PassId,
+        pos: Vec2,
+        bb: Quad,
+        text: &text::TextDisplay,
+        effects: &[text::Effect],
+        colors: &[color::Rgba],
+    ) {
+        todo!()
+    }
+}
--- crates/kas-soft/src/lib.rs
+++ crates/kas-soft/src/lib.rs
@@ -10,3 +10,70 @@
 //! This crate supports themes via the [`kas::theme`].
 //!
 //! [softbuffer]: https://github.com/rust-windowing/softbuffer
+
+mod draw;
+
+use std::time::Instant;
+
+pub use draw::Shared;
+use kas::draw::{DrawImpl, DrawSharedImpl, SharedState, WindowCommon, color};
+use kas::geom::Size;
+use kas::runner::{GraphicsInstance, RunError, WindowSurface};
+use raw_window_handle::{HasDisplayHandle, HasWindowHandle};
+
+/// Graphics context
+pub struct Instance {}
+
+impl Instance {
+    /// Construct a new `Instance`
+    pub fn new() -> Self {
+        Instance {}
+    }
+}
+
+pub struct Surface {}
+
+impl WindowSurface for Surface {
+    type Shared = Shared;
+
+    fn size(&self) -> Size {
+        todo!()
+    }
+
+    fn configure(&mut self, shared: &mut Shared, size: Size) -> bool {
+        todo!()
+    }
+
+    fn draw_iface<'iface>(
+        &'iface mut self,
+        shared: &'iface mut SharedState<Shared>,
+    ) -> kas::draw::DrawIface<'iface, Shared> {
+        todo!()
+    }
+
+    fn common_mut(&mut self) -> &mut WindowCommon {
+        todo!()
+    }
+
+    fn present(&mut self, shared: &mut Shared, clear_color: color::Rgba) -> Instant {
+        todo!()
+    }
+}
+
+impl GraphicsInstance for Instance {
+    type Shared = Shared;
+
+    type Surface<'a> = Surface;
+
+    fn new_shared(&mut self, surface: Option<&Surface>) -> Result<Shared, RunError> {
+        Ok(Shared {})
+    }
+
+    fn new_surface<'window, W>(&mut self, window: W, transparent: bool) -> Result<Surface, RunError>
+    where
+        W: HasWindowHandle + HasDisplayHandle + Send + Sync + 'window,
+        Self: Sized,
+    {
+        Ok(Surface {})
+    }
+}
