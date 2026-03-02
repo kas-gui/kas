9dcbc42cb166374a49a9f5f835cdb0382ae693b5
Diggory Hardy <git@dhardy.name>
2026-03-02 12:46:20 +0000

    Use #[split_impl] on ThemeDst

--- crates/kas-core/src/theme/theme_dst.rs
+++ crates/kas-core/src/theme/theme_dst.rs
@@ -18,62 +18,46 @@ use crate::theme::ThemeDraw;
 /// This trait is implemented automatically for all implementations of
 /// [`Theme`]. It is intended only for use where a less parameterised
 /// trait is required.
+#[crate::split_impl(for<T: Theme<DS>> T)]
 pub trait ThemeDst<DS: DrawSharedImpl> {
     /// Theme initialisation
     ///
     /// See also [`Theme::init`].
-    fn init(&mut self, config: &RefCell<Config>);
+    fn init(&mut self, config: &RefCell<Config>) {
+        self.init(config);
+    }
 
     /// Construct per-window storage
     ///
     /// See also [`Theme::new_window`].
-    fn new_window(&mut self, config: &WindowConfig) -> Box<dyn Window>;
+    fn new_window(&mut self, config: &WindowConfig) -> Box<dyn Window> {
+        let window = <T as Theme<DS>>::new_window(self, config);
+        Box::new(window)
+    }
 
     /// Update a window created by [`Theme::new_window`]
     ///
     /// See also [`Theme::update_window`].
     ///
     /// Returns `true` when a resize is required based on changes to the scale factor or font size.
-    fn update_window(&mut self, window: &mut dyn Window, config: &WindowConfig) -> bool;
+    fn update_window(&mut self, window: &mut dyn Window, config: &WindowConfig) -> bool {
+        let window = window.as_any_mut().downcast_mut().unwrap();
+        self.update_window(window, config)
+    }
 
     fn draw<'a>(
         &'a self,
         draw: DrawIface<'a, DS>,
         ev: &'a mut EventState,
         window: &'a mut dyn Window,
-    ) -> Box<dyn ThemeDraw + 'a>;
-
-    /// Background colour
-    ///
-    /// See also [`Theme::clear_color`].
-    fn clear_color(&self) -> color::Rgba;
-}
-
-impl<DS: DrawSharedImpl, T: Theme<DS>> ThemeDst<DS> for T {
-    fn init(&mut self, config: &RefCell<Config>) {
-        self.init(config);
-    }
-
-    fn new_window(&mut self, config: &WindowConfig) -> Box<dyn Window> {
-        let window = <T as Theme<DS>>::new_window(self, config);
-        Box::new(window)
-    }
-
-    fn update_window(&mut self, window: &mut dyn Window, config: &WindowConfig) -> bool {
-        let window = window.as_any_mut().downcast_mut().unwrap();
-        self.update_window(window, config)
-    }
-
-    fn draw<'b>(
-        &'b self,
-        draw: DrawIface<'b, DS>,
-        ev: &'b mut EventState,
-        window: &'b mut dyn Window,
-    ) -> Box<dyn ThemeDraw + 'b> {
+    ) -> Box<dyn ThemeDraw + 'a> {
         let window = window.as_any_mut().downcast_mut().unwrap();
         Box::new(<T as Theme<DS>>::draw(self, draw, ev, window))
     }
 
+    /// Background colour
+    ///
+    /// See also [`Theme::clear_color`].
     fn clear_color(&self) -> color::Rgba {
         self.clear_color()
     }
