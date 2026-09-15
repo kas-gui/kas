b8965ac9e5eb431bf79b379abfc1f235e5ca0363
Diggory Hardy <git@dhardy.name>
2026-09-15 18:05:55 +0000

    Re-export winit from kas_core

--- crates/kas-core/src/lib.rs
+++ crates/kas-core/src/lib.rs
@@ -20,6 +20,8 @@ extern crate self as kas;
 
 #[doc(inline)] pub extern crate easy_cast as cast;
 
+pub extern crate winit;
+
 // internal modules:
 #[cfg(feature = "accesskit")] pub(crate) mod accesskit;
 mod action;
